c690df3711f39dede008811c029dfb8c90c8773c
Luke Rogers <luke@dmptr.com>
2019-12-29 14:52:08 +1300

    [0.3.4] Add app config and debug-toolbar version.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

---
+++ markdownfield/__init__.py
@@ -0,0 +1,3 @@
+from .version import __version__  # noqa: F401
+
+default_app_config = 'markdownfield.apps.MarkdownFieldConfig'
---
+++ markdownfield/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MarkdownFieldConfig(AppConfig):
+    name = 'markdownfield'
+    verbose_name = 'Markdown Field'

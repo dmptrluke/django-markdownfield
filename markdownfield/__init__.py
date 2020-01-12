2a0e52613bdb11207e2723d71f8d1c38ddf75132
Luke Rogers <luke@dmptr.com>
2020-01-13 03:51:18 +1300

    [0.7.1] Code style changes, add a proper description for pypi.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- markdownfield/__init__.py
+++ markdownfield/__init__.py
@@ -1,4 +1,5 @@
-"""A markdown field."""
-__version__ = '0.7.0'
+"""A simple custom field for Django that can safely render Markdown and store it in the database.
+"""
+__version__ = '0.7.1'
 
 default_app_config = 'markdownfield.apps.MarkdownFieldConfig'

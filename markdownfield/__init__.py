9cd48ee84296b7c60bfc5b549a7b6098f9441c26
Luke Rogers <luke@dmptr.com>
2021-04-02 00:32:56 +1300

    Update EasyMDE

--- markdownfield/__init__.py
+++ markdownfield/__init__.py
@@ -1,5 +1,5 @@
 """A simple custom field for Django that can safely render Markdown and store it in the database.
 """
-__version__ = '0.9.0'
+__version__ = '0.10.0'
 
 default_app_config = 'markdownfield.apps.MarkdownFieldConfig'

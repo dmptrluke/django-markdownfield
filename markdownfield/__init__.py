4da1be88fa4f8af19132435784cebc98b499474e
Benjamin Balder Bach <benjamin@overtag.dk>
2023-08-24 23:27:02 +0200

    Get rid of RemovedInDjango41Warning (#11)
    * Get rid of RemovedInDjango41Warning
    
    ```
    .virtualenvs/dukop/lib/python3.8/site-packages/django/apps/registry.py:91: RemovedInDjango41Warning: 'markdownfield' defines default_app_config = 'markdownfield.apps.MarkdownFieldConfig'. Django now detects this configuration automatically. You can remove default_app_config.
        app_config = AppConfig.create(entry)
    ```
    
    * Bump support to Django>=3.2
    
    * Update __init__.py

--- markdownfield/__init__.py
+++ markdownfield/__init__.py
@@ -1,5 +1,3 @@
 """A simple custom field for Django that can safely render Markdown and store it in the database.
 """
-__version__ = '0.10.1'
-
-default_app_config = 'markdownfield.apps.MarkdownFieldConfig'
+__version__ = '0.11.0'
